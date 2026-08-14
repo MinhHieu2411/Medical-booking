e5f7fcec33bd81a8eaa47aeab8e487764cd1aa68
MinhHieu2411 <2051060508@e.tlu.edu.vn>
2026-08-14 18:21:52 +0700

    done frontend - begin backend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { use, useState } from 'react'
-import {assets} from '../assets/assets'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { assets } from '../assets/assets';
 
 const Navbar = () => {
 
@@ -45,7 +45,21 @@ const Navbar = () => {
           </div>
           : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
         }
-        
+        <img onClick={()=>setShowmenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+        {/* mobile menu */}
+        <div className={`  ${showMenu ? 'fixed w-full' : 'h-0 w-0'}  md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+          <div className='flex items-center justify-between px-5 py-6'>
+            <img className='w-36' src={assets.logo} alt="" />
+            <img className='w-7' onClick={()=>setShowmenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <NavLink onClick={()=>setShowmenu(false)} to='/'> <p  className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+            <NavLink onClick={()=>setShowmenu(false)} to='/doctors'><p  className='px-4 py-2 rounded inline-block'>All DOCTORS</p></NavLink>
+            <NavLink onClick={()=>setShowmenu(false)} to='/about'><p  className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+            <NavLink onClick={()=>setShowmenu(false)} to='/contact'><p  className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+          </ul>
+        </div>
+
       </div>
     </div>
   )
--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -35,7 +35,7 @@ const RelatedDoctors = ({ speciality, docId }) => {
           </div>
         ))}
       </div>
-      <button onClick={()=>{ navigate(`doctors/`); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10' >More...</button>
+      <button onClick={()=>{ navigate(`/doctors/`); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10' >More...</button>
     </div>
     </div>
   )
